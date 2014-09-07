b565ec04b5e29ed41b44f521be7fc92782de48a3
Jordan Hansen <jbhansen84@gmail.com>
2014-09-06 23:05:22 -0600

    fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
 
 	def getJson		
 		battle_tag = URI.encode(params[:battle_tag])
-		@account = HTTParty.get('https://us.api.battle.net/d3/profile/' + battle_tag + '/?locale=en_US&apikey=4dahhwwp6tt6pzsn52ctjawkqkqynhe7')
+		@account = HTTParty.get('https://us.api.battle.net/d3/profile/' + battle_tag + '/?locale=en_US&apikey=' + ENV['BNET_WEB_API_KEY'])
 
 		render :json => @account
 	end
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
 	def getHero
 		battle_tag = URI.encode(params[:battle_tag])
 
-		@hero = HTTParty.get('https://us.api.battle.net/d3/profile/melayish%231811/hero/'+ params[:hero_id] +'?locale=en_US&apikey=4dahhwwp6tt6pzsn52ctjawkqkqynhe7')
+		@hero = HTTParty.get('https://us.api.battle.net/d3/profile/melayish%231811/hero/'+ params[:hero_id] +'?locale=en_US&apikey=' + ENV['BNET_WEB_API_KEY'])
 
 		render :json => @hero
 	end
